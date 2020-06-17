3ee553a0f3abdcb649309e0b0c707d3904e3c1bd
Nicole (Deborah) Marcum <marcum531@gmail.com>
2020-06-16 21:04:54 -0400

    thirteenth upload

---
+++ VacationPy/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Weather API
+api_key = "0ef0e1e8cae0c9ce2fd55f511fb39ea6"
+
+#Google API
+gkey = "AIzaSyAcU_eGLIYWAT6OF27fRNAdFohgRzInBeA"
+
